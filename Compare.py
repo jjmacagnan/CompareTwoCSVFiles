ea25c217ce1c5ed058048f2a233b177719933882
jjmacagnan <jlmacagnan@gmail.com>
2021-07-13 11:35:19 -0300

    save

--- Compare.py
+++ Compare.py
@@ -1,12 +1,25 @@
-f1 = open("tests1.csv").readlines()
-f2 = open("tests2.csv").readlines()
+import os
 
+original_files_path = 'original_files'
+compared_files_path = 'compared_files'
 
-file = open('edge_histogram.csv', 'a')
-for row1 in f1:
-    # print(row1[:7])
-    for row2 in f2:
-        # print(row2[:7])
-        if row1[:19] == row2[:19]:
-            # print(row1)
-            file.write(row2)
+f1 = open("planilha.csv").readlines()
+
+files = os.listdir(original_files_path)
+
+for filename in files:
+    print(filename)
+
+    f2 = open(original_files_path + "/" + filename).readlines()
+
+    output_file = open(compared_files_path + "/" + filename, 'w')
+    for row1 in f1:
+        # print(row1[:7])
+        for row2 in f2:
+            # print(row2[:7])
+            if row1[:10] == row2[:10]:
+                # print(row1)
+                output_file.write(row2)
+
+    output_file.close()
+
