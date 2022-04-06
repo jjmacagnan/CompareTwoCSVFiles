f2881e286a74bad63bcfc24332ad8e5813d11ad2
jjmacagnan <jlmacagnan@gmail.com>
2022-04-06 11:04:03 -0300

    save

--- Compare.py
+++ Compare.py
@@ -3,23 +3,25 @@ import os
 original_files_path = 'original_files'
 compared_files_path = 'compared_files'
 
-f1 = open("planilha.csv").readlines()
+f1 = open("AND_test_metadata_mammoset_ddsm_birads_class_7.csv").readlines()
 
 files = os.listdir(original_files_path)
 
 for filename in files:
-    print(filename)
+    # print(filename)
 
-    f2 = open(original_files_path + "/" + filename).readlines()
+    if not filename.startswith('.'):
+        print(filename)
 
-    output_file = open(compared_files_path + "/" + filename, 'w')
-    for row1 in f1:
-        # print(row1[:7])
-        for row2 in f2:
-            # print(row2[:7])
-            if row1[:10] == row2[:10]:
-                # print(row1)
-                output_file.write(row2)
+        f2 = open(original_files_path + "/" + filename).readlines()
 
-    output_file.close()
+        output_file = open(compared_files_path + "/" + filename, 'w')
+        for row1 in f1:
+            # print(row1[:7])
+            for row2 in f2:
+                # print(row2[:7])
+                if row1[:16] == row2[:16]:
+                    # print(row1)
+                    output_file.write(row2)
 
+        output_file.close()
